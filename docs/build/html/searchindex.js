4d277435cc6fd3d0b11b0282e288856f2e7e0ef3
Dan <danferguson@outlook.com>
2018-06-27 11:57:19 -0700

    added sphinx documentation

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Finally, version control for Windows!"],titleterms:{"final":0,control:0,document:[],indic:0,tabl:0,trashshel:[],version:0,welcom:[],window:0}})
---
+++ docs/source/index.rst
@@ -0,0 +1,15 @@
+Finally, version control for Windows!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
