7a2e920287dcbd19f1e862f5b0dd011fd16103cd
Dan Ferguson <dan.ferguson@cidresearch.org>
2018-10-19 09:57:56 -0700

    added login-exch-online.ps1

---
+++ exch/online/login-exch-online.ps1
@@ -0,0 +1,9 @@
+﻿# Connect PowerShell session to Exchange Online
+# Script created from article:
+# <https://docs.microsoft.com/en-us/powershell/exchange/exchange-online/connect-to-exchange-online-powershell/connect-to-exchange-online-powershell?view=exchange-ps>
+
+$LiveCred = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $LiveCred -Authentication Basic –AllowRedirection
+
+Import-PSSession $Session
